fc81d26ec80eadbc5ce352cae301f0c07336c7be
Shanyu Juneja <30368557+terminalsin@users.noreply.github.com>
2022-04-28 10:39:40 +0200

    Create installation-post.sh

---
+++ scripts/installation-post.sh
@@ -0,0 +1,6 @@
+sudo apt install wget unzip
+cd ~
+wget https://github.com/Phala-Network/solo-mining-scripts/archive/refs/heads/main.zip
+unzip main.zip
+cd ~/solo-mining-scripts-main 
+sudo ./phala.sh install
