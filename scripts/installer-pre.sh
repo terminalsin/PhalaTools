52166bb3ac540cd6d60a6547860bcb905e802d00
Shanyu Juneja <30368557+terminalsin@users.noreply.github.com>
2022-04-28 10:24:53 +0200

    Create installer-pre.sh

---
+++ scripts/installer-pre.sh
@@ -0,0 +1,7 @@
+sudo apt-get update
+sudo apt-get install -y linux-image-5.8.0-63-generic linux-headers-5.8.0-63-generic linux-modules-extra-5.8.0-63-generic git
+cd /root/
+git clone https://github.com/mtompkins/linux-kernel-utilities.git
+cd linux-kernel-utilities/
+chmod +x *.sh
+reboot
