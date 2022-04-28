50d3cdc0e7a753c90b493aa08213a28d40116670
Shanyu Juneja <30368557+terminalsin@users.noreply.github.com>
2022-04-28 10:41:16 +0200

    Update installer-post.sh

--- scripts/installer-post.sh
+++ scripts/installer-post.sh
@@ -1,4 +1,4 @@
-sudo apt install wget unzip
+sudo apt install -y wget unzip software-properties-common
 cd ~
 wget https://github.com/Phala-Network/solo-mining-scripts/archive/refs/heads/main.zip
 unzip main.zip
