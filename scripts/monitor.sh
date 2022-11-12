bc3d24d75712a043a4efd730aca6939b23cd5807
Shanyu Thibaut Juneja <30368557+terminalsin@users.noreply.github.com>
2022-11-12 11:56:52 -0500

    Create monitor.sh

---
+++ scripts/monitor.sh
@@ -0,0 +1,22 @@
+curl -s https://api.github.com/repos/prometheus/node_exporter/releases/latest | grep browser_download_url | grep linux-amd64 |  cut -d '"' -f 4 | wget -qi -
+tar xvf node_exporter-*linux-amd64.tar.gz
+cd node_exporter*/
+sudo mv node_exporter /usr/local/bin/
+
+cat <<EOT >> /etc/systemd/system/node_exporter.service
+[Unit]
+Description=Node Exporter
+After=network.target
+
+[Service]
+User=node_exporter
+Group=node_exporter
+Type=simple
+ExecStart=/usr/local/bin/node_exporter
+
+[Install]
+WantedBy=multi-user.target
+EOT
+
+sudo systemctl start node_exporter
+sudo systemctl enable node_exporter
